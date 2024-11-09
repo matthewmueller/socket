f06fcfd0b8637b0526d95a8fb8b1b7b6433f5cec
Matthew Mueller <mattmuelle@gmail.com>
2024-11-09 02:00:24 -0600

    tweaks to variable names, comments and filenames

--- socket.go
+++ socket.go
@@ -10,8 +10,8 @@ import (
 	"os/signal"
 )
 
-// Listen creates a new listener based on the path
-func Listen(path string) (net.Listener, error) {
+// Listen creates a new listener based on the addr
+func Listen(addr string) (net.Listener, error) {
 	// Try activating the socket first
 	lns, err := maybeActivate()
 	if err != nil {
@@ -19,18 +19,18 @@ func Listen(path string) (net.Listener, error) {
 	} else if len(lns) > 0 {
 		return lns[0], nil
 	}
-	// Otherwise create a new listener based on the path
-	url, err := Parse(path)
+	// Otherwise create a new listener based on the addr
+	url, err := Parse(addr)
 	if err != nil {
 		return nil, err
 	}
-	// Empty host means the path is a unix domain socket
+	// Empty host means the addr is a unix domain socket
 	if url.Host == "" {
-		// Unix domain socket path can't be more than 103 characters long
-		if len(path) > 103 {
-			return nil, fmt.Errorf("socket: unix path too long %q", path)
+		// Unix domain socket addr can't be more than 103 characters long
+		if len(addr) > 103 {
+			return nil, fmt.Errorf("socket: unix path too long %q", addr)
 		}
-		addr, err := net.ResolveUnixAddr("unix", path)
+		addr, err := net.ResolveUnixAddr("unix", addr)
 		if err != nil {
 			return nil, err
 		}
@@ -41,18 +41,19 @@ func Listen(path string) (net.Listener, error) {
 		return unix, nil
 	}
 	// Otherwise, we listen on a TCP port
-	addr, err := net.ResolveTCPAddr("tcp", url.Host)
+	tcpAddr, err := net.ResolveTCPAddr("tcp", url.Host)
 	if err != nil {
 		return nil, err
 	}
-	tcp, err := net.ListenTCP("tcp", addr)
+	tcp, err := net.ListenTCP("tcp", tcpAddr)
 	if err != nil {
 		return nil, err
 	}
 	return tcp, nil
 }
 
-// Serve the handler at address
+// Serve the handler at address. // When the context is canceled, the server
+// will be gracefully shutdown.
 func Serve(ctx context.Context, listener net.Listener, handler http.Handler) error {
 	// Create the HTTP server
 	server := &http.Server{
@@ -60,7 +61,7 @@ func Serve(ctx context.Context, listener net.Listener, handler http.Handler) err
 		Handler: handler,
 	}
 	// Make the server shutdownable
-	shutdown := shutdown(ctx, server)
+	shutdownCh := shutdown(ctx, server)
 	// Serve requests
 	if err := server.Serve(listener); err != nil {
 		if !errors.Is(err, http.ErrServerClosed) {
@@ -68,7 +69,7 @@ func Serve(ctx context.Context, listener net.Listener, handler http.Handler) err
 		}
 	}
 	// Handle any errors that occurred while shutting down
-	if err := <-shutdown; err != nil {
+	if err := <-shutdownCh; err != nil {
 		if !errors.Is(err, context.Canceled) {
 			return err
 		}
@@ -76,9 +77,10 @@ func Serve(ctx context.Context, listener net.Listener, handler http.Handler) err
 	return nil
 }
 
-// ListenAndServe is a convenience function that combines Listen and Serve
-func ListenAndServe(ctx context.Context, path string, handler http.Handler) error {
-	ln, err := Listen(path)
+// ListenAndServe is a convenience function that combines Listen and Serve.
+// When the context is canceled, the server will be gracefully shutdown.
+func ListenAndServe(ctx context.Context, addr string, handler http.Handler) error {
+	ln, err := Listen(addr)
 	if err != nil {
 		return err
 	}
@@ -90,7 +92,8 @@ func shutdown(ctx context.Context, server *http.Server) <-chan error {
 	shutdown := make(chan error, 1)
 	go func() {
 		<-ctx.Done()
-		// Wait for one more interrupt to force an immediate shutdown
+		// Wait for one more interrupt to force an immediate shutdown, otherwise
+		// take as much time as needed to finish ongoing requests
 		forceCtx := trap(context.Background(), os.Interrupt)
 		if err := server.Shutdown(forceCtx); err != nil {
 			shutdown <- err
